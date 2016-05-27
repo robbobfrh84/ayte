7c91cc203d95f02320a6d0c518406bac8280dd30
Bob Main <bobmain49@gmail.com>
2016-05-27 13:27:37 -0700

    e

--- live/gal.js
+++ live/gal.js
@@ -5,19 +5,19 @@ var skip = [0,1,21,27,28,29,32];
 
 function swapToGal(){
   if (!stateGal){ stateGal = true;
-    if (live) {liveStatus();} 
+    if (live) {liveStatus();}
     console.log('swapped to gallery Page');
+    gal1Frame.style.display = 'block';
     ayteFrame.style.display = 'none';
     paletteFrame.style.display = 'none';
-    // liveBtn.style.display = 'none';
-    gal1.style.display = 'block';
+    //gal1.style.display = 'block';
     galBtn.innerHTML = '<-Back'
     historyGal.flex_history(getAll, flex_history_callback);
   } else { stateGal = false;
+    gal1Frame.style.display = 'none';
     ayteFrame.style.display = 'block';
     paletteFrame.style.display = 'block';
-    // liveBtn.style.display = 'inLine-block';
-    gal1.style.display = 'none';
+    //gal1.style.display = 'none';
     galBtn.innerHTML = '{Gallery}'
 
   }
@@ -27,38 +27,56 @@ var w = 8; //var w=15; var gap=1.5; var rnd=3; LOOKS GOOD FOR MEDIUM THUMB
 var gap = -0.75;
 var rnd = 0; //remove!!!
 var filter = 'url(#f1)';
-function buildGal(){
-  var col = -1; var row = -8;
-  for (var i = 0; i < allGalHistory[0].length; i++){
-    if (i % 8 === 0){ row+=8; col++}
-    //need to build a container so that we can have click effects....
 
-    buildThumbnail(i, (i-row)*((w+gap)*10), ((w+gap)*10)*col);
-  }
-}
+function buildGal(){}
+//   for (var i = 0; i < allGalHistory[0].length; i++){
+//
+//
+//
+//     if (i % 8 === 0){ ayteFrame.innerHTML += '<br>';}
+//
+//     var blk = document.createElement('div');
+//     blk.className = 'ayte'; blk.id = 'blk'+i;
+//     blk.setAttribute('onmousedown', 'mouseState(true, blk'+i+','+i+')');
+//     blk.setAttribute('onmousemove', 'draw(blk'+i+','+i+')');
+//     blk.setAttribute('ontouchstart','draw(blk'+i+','+i+')');
+//     ayteFrame.appendChild(blk);
+//   }
+// }
 
-function buildThumbnail(ayte, cA, rA){
-  var col = 0; var row = -8;
-  for (var i = 0; i < 64; i++){
-    if (i % 8 === 0){ row+=8; col++}
-    var c = allGalHistory[ayte][i];
-    if (c !== "" && c !== 'rgb(221, 221, 221)'){
-        createRect('gal1',cA+((w+gap)*(i-row)),rA+((w+gap)*col),w,w,rnd,rnd
-        ,c,0,'none',filter,'aytep'+i);
-    }
-  }
-}
 
-function createRect(container,x,y,width,height,rx,ry,fill,bordWidth,bordColor,filter,recID){
-  var newEl = createEl(container,'rect',[['id',recID],['stroke-width',bordWidth+'px']
-  ,['stroke',bordColor],['x',x+'px'],['y', y+'px'],['width',width+'px'],['height',height+'px']
-  ,['filter', filter]
-  ,['rx',rx],['ry',ry],['fill',fill]]); return newEl;}
-
-function createEl(container,type,att){
-  var newObj = document.createElementNS(svgElement, type);
-  for (var i=0; i<att.length; i++){ newObj.setAttributeNS(null, att[i][0],att[i][1]); }
-  document.getElementById(container).appendChild(newObj); return newObj; }
+// function buildGal(){
+//   var col = -1; var row = -8;
+//   for (var i = 0; i < allGalHistory[0].length; i++){
+//     if (i % 8 === 0){ row+=8; col++}
+//     //need to build a container so that we can have click effects....
+//     var container = 'gal1';
+//     buildThumbnail(container, i, (i-row)*((w+gap)*10), ((w+gap)*10)*col);
+//   }
+// }
+//
+// function buildThumbnail(container, ayte, cA, rA){
+//   var col = 0; var row = -8;
+//   for (var i = 0; i < 64; i++){
+//     if (i % 8 === 0){ row+=8; col++}
+//     var c = allGalHistory[ayte][i];
+//     if (c !== "" && c !== 'rgb(221, 221, 221)'){
+//         createRect('gal1',cA+((w+gap)*(i-row)),rA+((w+gap)*col),w,w,rnd,rnd
+//         ,c,0,'none',filter,'aytep'+i);
+//     }
+//   }
+// }
+//
+// function createRect(container,x,y,width,height,rx,ry,fill,bordWidth,bordColor,filter,recID){
+//   var newEl = createEl(container,'rect',[['id',recID],['stroke-width',bordWidth+'px']
+//   ,['stroke',bordColor],['x',x+'px'],['y', y+'px'],['width',width+'px'],['height',height+'px']
+//   ,['filter', filter]
+//   ,['rx',rx],['ry',ry],['fill',fill]]); return newEl;}
+//
+// function createEl(container,type,att){
+//   var newObj = document.createElementNS(svgElement, type);
+//   for (var i=0; i<att.length; i++){ newObj.setAttributeNS(null, att[i][0],att[i][1]); }
+//   document.getElementById(container).appendChild(newObj); return newObj; }
 
 function isInArray(value, array) {
   return array.indexOf(value) > -1;

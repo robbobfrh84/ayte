8b61c446396c8adb5602ea75f1efa6d9ca7a4b98
Bob Main <bobmain49@gmail.com>
2016-05-28 11:39:49 -0700

    e

--- live/gal.js
+++ live/gal.js
@@ -31,7 +31,7 @@ var svgStart = '<svg xmlns="http://www.w3.org/2000/svg" width="10em" height="10e
 
 var shaddow = '<defs><filter id="f1" height="130%" width="130%"><feGaussianBlur in="SourceAlpha" stdDeviation="5"/> <feOffset dx="5" dy="5" result="offsetblur"/><feComponentTransfer><feFuncA type="linear" slope="0.5"/></feComponentTransfer><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs>';
 var newgroup = '<g filter="url(#f1)" >';
-
+//nooh
 function buildGal(){
   for (var i = 0; i < gCnt; i++){
     if (i % 8 === 0){ gal1Frame.innerHTML += '<br>';}
